aec771ed74568aaa52b1dcdb6c0829e97c9eed56
Kantaphat PHAPHUI <kantaphat.pha@ku.th>
2025-02-01 22:54:34 +0700

    Create LocationCoordinateDisplay of lon and lat

--- app/src/main/java/org/training/whatsup/NearMeScreen.kt
+++ app/src/main/java/org/training/whatsup/NearMeScreen.kt
@@ -3,8 +3,11 @@ package org.training.whatsup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import org.training.whatsup.ui.theme.WhatsUpTheme
 
 @Composable
@@ -28,6 +33,7 @@ fun NearMeScreen() {
             ) {
                 // Column Content
                 Text(text = "Events Near Me")
+                LocationCoordinateDisplay(lat = "0.0", lon = "0.0")
             } //end Column
         } //end Surface
     } //end Theme
@@ -38,3 +44,48 @@ fun NearMeScreen() {
 fun NearMeScreenPreview() {
     NearMeScreen()
 }
+
+@Composable
+fun LocationCoordinateDisplay(lat: String, lon: String) {
+    ConstraintLayout (
+        modifier = Modifier.fillMaxWidth(1f).padding(all = 8.dp)
+    ) {
+        val (goBtn, latField, lonField) = createRefs()
+        Button(
+            onClick = { /* TODO */ },
+            modifier = Modifier.constrainAs(goBtn) {
+                top.linkTo(parent.top, margin = 8.dp)
+                end.linkTo(parent.end, margin = 0.dp)
+                width = Dimension.wrapContent
+            }
+        ) {
+            Text(text = "GO")
+        } //end Button
+        OutlinedTextField(
+            value = lat,
+            label = {
+                Text(text = "Latitude")
+            },
+            onValueChange = {},
+            modifier = Modifier.constrainAs(latField) {
+                top.linkTo(parent.top, margin = 0.dp)
+                start.linkTo(parent.start, margin = 0.dp)
+                end.linkTo(goBtn.start, margin = 8.dp)
+                width = Dimension.fillToConstraints
+            }
+        ) //end lat
+        OutlinedTextField(
+            value = lon,
+            label = {
+                Text(text = "Longitude")
+            },
+            onValueChange = {},
+            modifier = Modifier.constrainAs(lonField) {
+                top.linkTo(latField.bottom, margin = 0.dp)
+                start.linkTo(parent.start, margin = 0.dp)
+                end.linkTo(goBtn.start, margin = 8.dp)
+                width = Dimension.fillToConstraints
+            }
+        ) //end lon
+    } //end ConstraintLayout
+} //end LocationCoordinate
